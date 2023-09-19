37098abefe260a4b4faef818ffc18ea83e7ee4fd
Restyled.io <commits@restyled.io>
2023-09-19 14:15:22 +0000

    Restyled by autopep8

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@
 # - Know if the hosts are up or not             :tick
 # - Launch custom command on selected host      :tick
 #
-# - Docker generic -> info about runners and containers 
+# - Docker generic -> info about runners and containers
 
 # Thinking about using the same way of launching remote command (or playbooks) to control docker containers
 # Using ansible_runner the docker lib is not necessary, but it will be used for other things
@@ -37,58 +37,80 @@ global CHAT_ID
 # Version 0.1 will be able to launch ansible playbooks on remote hosts
 
 # pass a host is unnecessary, the playbook will do it in the hosts section of the yaml file
+
+
 def playbook_runner(playbook_name):
-        r = ansible_runner.run(private_data_dir='/.ansible/playbooks', playbook=playbook_name) # -> not the definitive path but for now it will do
-        #print("{}: {}".format(r.status, r.rc))
-        return r.status, r.rc # -> this will return the status and the return code of the playbook
+    # -> not the definitive path but for now it will do
+    r = ansible_runner.run(
+        private_data_dir='/.ansible/playbooks', playbook=playbook_name)
+    # print("{}: {}".format(r.status, r.rc))
+    # -> this will return the status and the return code of the playbook
+    return r.status, r.rc
+
 
 def check_ups_battery(self):
     global CHAT_ID
     # controll if .ups_battery.alert exists
-    if(exists("/.ups_battery.alert")):
+    if (exists("/.ups_battery.alert")):
         # send text to admin
-        self.bot.sendMessage(CHAT_ID, "Ups battery mode on, sending shutdown command to all hosts")
+        self.bot.sendMessage(
+            CHAT_ID, "Ups battery mode on, sending shutdown command to all hosts")
         # send shutdown command to all hosts
         status, rc = playbook_runner("shutdown_ups.yml")
         msg_return = "Status: " + status + "\nReturn code: " + rc
         self.bot.sendMessage(CHAT_ID, msg_return)
         # delete .ups_battery.alert
-        subprocess.check_output("rm /.ups_battery.alert", shell=True, stderr=subprocess.STDOUT)
+        subprocess.check_output("rm /.ups_battery.alert",
+                                shell=True, stderr=subprocess.STDOUT)
     else:
         pass
 
+
 def docker_ps():
-    response = subprocess.check_output(f"ansible all -m shell -a 'docker ps'", shell=True, stderr=subprocess.STDOUT) # ps ha to run on all hosts
+    response = subprocess.check_output(
+        f"ansible all -m shell -a 'docker ps'", shell=True, stderr=subprocess.STDOUT)  # ps ha to run on all hosts
     return response
+
 
 def docker_start(host, container_name):
-    response = subprocess.check_output(f"ansible {host} -m shell -a 'docker run -d {container_name}'", shell=True, stderr=subprocess.STDOUT) # ps ha to run on all hosts
+    response = subprocess.check_output(f"ansible {host} -m shell -a 'docker run -d {container_name}'",
+                                       shell=True, stderr=subprocess.STDOUT)  # ps ha to run on all hosts
     return response
+
 
 def docker_stop(host, container_name):
-    response = subprocess.check_output(f"ansible {host} -m shell -a 'docker stop {container_name}'", shell=True, stderr=subprocess.STDOUT) # ps ha to run on all hosts
+    response = subprocess.check_output(
+        f"ansible {host} -m shell -a 'docker stop {container_name}'", shell=True, stderr=subprocess.STDOUT)  # ps ha to run on all hosts
     return response
+
 
 def docker_health(host, container_name):
-    response = subprocess.check_output(f"ansible {host} -m shell -a 'docker inspect {container_name}'", shell=True, stderr=subprocess.STDOUT) # ps ha to run on all hosts
+    response = subprocess.check_output(f"ansible {host} -m shell -a 'docker inspect {container_name}'",
+                                       shell=True, stderr=subprocess.STDOUT)  # ps ha to run on all hosts
     return response
+
 
 def docker_info(host):
-    response = subprocess.check_output(f"ansible {host} -m shell -a 'docker stats'", shell=True, stderr=subprocess.STDOUT) # ps ha to run on all hosts
+    response = subprocess.check_output(
+        f"ansible {host} -m shell -a 'docker stats'", shell=True, stderr=subprocess.STDOUT)  # ps ha to run on all hosts
     return response
+
 
 def docker_images(host):
-    response = subprocess.check_output(f"ansible {host} -m shell -a 'docker images'", shell=True, stderr=subprocess.STDOUT) # ps ha to run on all hosts
+    response = subprocess.check_output(
+        f"ansible {host} -m shell -a 'docker images'", shell=True, stderr=subprocess.STDOUT)  # ps ha to run on all hosts
     return response
 
+
 def ups_control():
-        # Control if ups_battery.alert exists
-            #if so return battery mode on
-            # else return normal state
-        if(exists("/.ups_battery.alert")):
-            return "battery mode on"
-        else:
-            return "normal state"
+    # Control if ups_battery.alert exists
+    # if so return battery mode on
+    # else return normal state
+    if (exists("/.ups_battery.alert")):
+        return "battery mode on"
+    else:
+        return "normal state"
+
 
 def host_up_controll():
     # use a host file and send a ping
@@ -96,15 +118,20 @@ def host_up_controll():
         host_list = host_file.read().split("\n")
     responses = {}
     for host in host_list:
-        responses[host] = subprocess.check_output(f"ping -c 1 {host}", shell=True, stderr=subprocess.STDOUT)
+        responses[host] = subprocess.check_output(
+            f"ping -c 1 {host}", shell=True, stderr=subprocess.STDOUT)
 
     return responses
 
-def custom_command_runner(host, command): #not much but works so far
+
+def custom_command_runner(host, command):  # not much but works so far
     # structure of the command
-        # sudo ansible <host/group> -m shell -a "<command>"
-    response = subprocess.check_output(f"ansible {host} -m shell -a {command}", shell=True, stderr=subprocess.STDOUT) # not the best way but using ansible is easier than paramiko in this case
+    # sudo ansible <host/group> -m shell -a "<command>"
+    # not the best way but using ansible is easier than paramiko in this case
+    response = subprocess.check_output(
+        f"ansible {host} -m shell -a {command}", shell=True, stderr=subprocess.STDOUT)
     return response
+
 
 class TelegramBot:
     def __init__(self, bot_token):
@@ -112,66 +139,69 @@ class TelegramBot:
         self.logger = logging.getLogger('TelegramBot')
         self.logger.setLevel(logging.INFO)
         self.setup_logger()
-    
+
     def setup_logger(self):
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
         # Log to console
         ch = logging.StreamHandler()
         ch.setFormatter(formatter)
         self.logger.addHandler(ch)
-    
+
     def handle_message(self, msg):
         global CHAT_ID
         content_type, chat_type, chat_id = telepot.glance(msg)
         CHAT_ID = chat_id
         message = msg['text'].split(" ")
-        if(message[0] == "/run"):
+        if (message[0] == "/run"):
             status, rc = playbook_runner(f"{message[1]}.yml")
             msg_return = "Status: " + status + "\nReturn code: " + rc
             self.bot.sendMessage(chat_id, msg_return)
-        elif(message[0] == "/battery"):
+        elif (message[0] == "/battery"):
             response = ups_control()
             self.bot.sendMessage(chat_id, response)
-        elif(message[0] == "/up"):
+        elif (message[0] == "/up"):
             reponses = host_up_controll()
             self.bot.sendMessage(chat_id, reponses)
-        elif(message[0] == "/command"):
-            msg = custom_command_runner(message[1], f"'{message[2]}'")     
+        elif (message[0] == "/command"):
+            msg = custom_command_runner(message[1], f"'{message[2]}'")
             self.bot.sendMessage(chat_id, msg)
         # Docker commands
-        elif(message[0] == "/docker"):
-            if(message[1] == "ps"):
+        elif (message[0] == "/docker"):
+            if (message[1] == "ps"):
                 response = docker_ps()
                 self.bot.sendMessage(chat_id, response)
-            elif(message[1] == "start"):
+            elif (message[1] == "start"):
                 response = docker_start(message[2], message[3])
                 self.bot.sendMessage(chat_id, response)
-            elif(message[1] == "stop"):
+            elif (message[1] == "stop"):
                 response = docker_stop(message[2], message[3])
                 self.bot.sendMessage(chat_id, response)
-            elif(message[1] == "health"):
+            elif (message[1] == "health"):
                 response = docker_health(message[2], message[3])
                 self.bot.sendMessage(chat_id, response)
-            elif(message[1] == "info"):
+            elif (message[1] == "info"):
                 response = docker_info(message[2])
                 self.bot.sendMessage(chat_id, response)
-            elif(message[1] == "images"):
+            elif (message[1] == "images"):
                 response = docker_images(message[2])
                 self.bot.sendMessage(chat_id, response)
             else:
                 self.bot.sendMessage(chat_id, "Command not found")
-    
+
     def start(self):
         self.bot.message_loop(self.handle_message)
         self.logger.info('Bot is listening...')
         while True:
             try:
                 time.sleep(1800)
-                check_ups_battery(self) # -> this will work as a listener for the ups battery activity
+                # -> this will work as a listener for the ups battery activity
+                check_ups_battery(self)
             except KeyboardInterrupt:
                 self.logger.info('Bot stopped')
                 exit()
+
 
 if __name__ == '__main__':
     bot = TelegramBot(TOKEN)
